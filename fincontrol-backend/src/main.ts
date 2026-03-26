88d4f1d1f923a2ddca481ff7303275739f9e6b17
William Correia <Williamcneto2015@gmail.com>
2026-03-26 16:59:17 -0300

    fix: corrigindo arquivos trocados entre frontend e backend

--- fincontrol-backend/src/main.ts
+++ fincontrol-backend/src/main.ts
@@ -4,22 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 🚀 CONFIGURAÇÃO DE CORS PARA PRODUÇÃO
+  // 🚀 CORS NO MODO "BALA DE PRATA" (O lugar certo é aqui no Backend!)
   app.enableCors({
-    // 1. Diga exatamente quem pode acessar (Substitua pela sua URL real da Vercel)
-    origin: [
-      'https://fincontrol-mu-lyart.vercel.app/login', // Sua URL de Produção (SEM a barra '/' no final)
-      'http://localhost:3000'                   // Mantém funcionando na sua máquina local
-    ],
-    // 2. Quais métodos HTTP são permitidos (incluindo o OPTIONS do preflight)
+    origin: true, 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    // 3. Permite o envio de tokens e cookies entre domínios diferentes
     credentials: true,
-    // 4. Quais cabeçalhos o frontend pode enviar
-    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
-  // O Railway injeta a porta automaticamente na variável process.env.PORT
   await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
--- fincontrol-frontend/services/api.ts
+++ fincontrol-frontend/services/api.ts
@@ -1,15 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://natural-liberation-production-27cf.up.railway.app', 
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
+  // Aqui vai a URL do Railway, e só isso!
+  baseURL: 'https://natural-liberation-production-27cf.up.railway.app',
 });
