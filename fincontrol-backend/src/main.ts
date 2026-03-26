8bd26023f786cd1e71ba45495f6fcd7a94a862c1
William Correia <Williamcneto2015@gmail.com>
2026-03-26 16:43:14 -0300

    fix: liberando CORS para a URL da Vercel

--- fincontrol-backend/src/main.ts
+++ fincontrol-backend/src/main.ts
@@ -1,19 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // Ativa o CORS para permitir que o Frontend (Next.js) comunique com a API
-  app.enableCors();
 
-  // Ativa a validação de dados em toda a aplicação
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Remove automaticamente campos extra que não estejam no DTO
-    forbidNonWhitelisted: true, // Dá erro se o utilizador enviar campos que não existem
-  }));
+  // 🚀 CONFIGURAÇÃO DE CORS PARA PRODUÇÃO
+  app.enableCors({
+    // 1. Diga exatamente quem pode acessar (Substitua pela sua URL real da Vercel)
+    origin: [
+      'https://fincontrol-mu-lyart.vercel.app/login', // Sua URL de Produção (SEM a barra '/' no final)
+      'http://localhost:3000'                   // Mantém funcionando na sua máquina local
+    ],
+    // 2. Quais métodos HTTP são permitidos (incluindo o OPTIONS do preflight)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    // 3. Permite o envio de tokens e cookies entre domínios diferentes
+    credentials: true,
+    // 4. Quais cabeçalhos o frontend pode enviar
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
 
+  // O Railway injeta a porta automaticamente na variável process.env.PORT
   await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
