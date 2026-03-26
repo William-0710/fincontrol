7916fa409a7c14ebef8a5435dfac6293ebd45e1b
William Correia <Williamcneto2015@gmail.com>
2026-03-26 16:51:03 -0300

    fix: alterando baseURL da api para o Railway

--- fincontrol-frontend/services/api.ts
+++ fincontrol-frontend/services/api.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: 'https://natural-liberation-production-27cf.up.railway.app', 
 });
 
 api.interceptors.request.use((config) => {
